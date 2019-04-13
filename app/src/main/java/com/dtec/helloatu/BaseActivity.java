d838e5fbdb2dbcb841405230528a1760e3e328fb
Rokan Uddin <ibrar.tanim@gmail.com>
2019-04-13 07:32:43 +0600

    First comit for uping the app

---
+++ app/src/main/java/com/dtec/helloatu/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.dtec.helloatu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
